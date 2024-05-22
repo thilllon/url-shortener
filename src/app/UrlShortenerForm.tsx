8cbe43dba01c60567f635965ac7d094b951473c2
thilllon <thilllon970@gmail.com>
2024-05-22 22:41:21 +0900

    feat: add shadcn

--- src/app/UrlShortenerForm.tsx
+++ src/app/UrlShortenerForm.tsx
@@ -1,41 +1,76 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+// import { toast } from '@/components/ui/use-toast';
+import { zodResolver } from '@hookform/resolvers/zod';
 import Link from 'next/link';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const FormSchema = z.object({
+  url: z.string().url().min(7, {
+    message: 'URL must be at least 7 characters.',
+  }),
+});
 
 export function UrlShortenerForm() {
-  const [url, setUrl] = useState('');
   const [shortUrl, setShortUrl] = useState('');
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      url: '',
+    },
+  });
+
+  function onSubmit({ url }: z.infer<typeof FormSchema>) {
+    const delimeter = '/s/';
+    if (!url || !['https://', 'http://'].some((scheme) => url.startsWith(scheme))) {
+      return;
+    }
+    fetch('/api/short', { method: 'POST', body: JSON.stringify({ url }) })
+      .then((response) => response.text())
+      .then((short) => {
+        setShortUrl(`${window.location.origin}${delimeter}${short}`);
+        form.reset();
+      })
+      .catch((error) => {
+        console.error(error);
+        setShortUrl('');
+      });
+  }
 
   return (
     <>
-      <form
-        onSubmit={async (event) => {
-          event.preventDefault();
-          const delimeter = '/s/';
-          if (!url || !['https://', 'http://'].some((scheme) => url.startsWith(scheme))) {
-            return;
-          }
-          fetch('/api/short', { method: 'POST', body: JSON.stringify({ url }) })
-            .then((response) => response.text())
-            .then((short) => {
-              setShortUrl(`${window.location.origin}${delimeter}${short}`);
-              setUrl('');
-            })
-            .catch((error) => {
-              console.error(error);
-              setShortUrl(error?.message);
-            });
-        }}
-        className='flex justify-center items-center gap-2 h-30'>
-        <input value={url} onChange={(ev) => setUrl(ev.target.value)} className='h-10 rounded-lg px-4 w-96' />
-        <button type='submit' className='h-10   border-black border-2 px-2 rounded-lg'>
-          {'create'}
-        </button>
-      </form>
-      <div>
-        <Link href={shortUrl}>{shortUrl}</Link>
-      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='max-w-[40rem] w-full mx-16 '>
+          <FormField
+            control={form.control}
+            name='url'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{'URL shortener'}</FormLabel>
+                <FormControl>
+                  <Input placeholder='Enter URL to shorten' {...field} />
+                </FormControl>
+                <Link href={shortUrl}>
+                  <FormDescription>{shortUrl}</FormDescription>
+                </Link>
+                <FormDescription>
+                  <Link href={shortUrl}>{shortUrl}</Link>
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type='submit' className='w-full'>
+            {'Submit'}
+          </Button>
+        </form>
+      </Form>
     </>
   );
 }
--- src/app/api/short/route.ts
+++ src/app/api/short/route.ts
@@ -1,18 +1,13 @@
 import { saveUrl } from '../../lib';
 
 export async function POST(request: Request) {
-  try {
-    const { url } = JSON.parse(await request.text());
-    if (!url) {
-      return new Response('Empty URL', { status: 400 });
-    }
-    if (['https://', 'http://'].some((scheme) => url.startsWith(scheme))) {
-      const short = await saveUrl(url);
-      return new Response(short, { status: 201 });
-    }
-    return new Response('Invalid URL', { status: 400 });
-  } catch (error) {
-    console.error(error);
-    return new Response('/', { status: 500 });
+  const { url } = JSON.parse(await request.text());
+  if (!url) {
+    throw new Error('Empty URL');
   }
+  if (['https://', 'http://'].some((scheme) => url.startsWith(scheme))) {
+    const short = await saveUrl(url);
+    return new Response(short);
+  }
+  throw new Error('Invalid URL');
 }
